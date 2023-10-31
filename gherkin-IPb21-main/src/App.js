c5bdeb08a6687057d815f84eb0f4b6608f90937b
ipb21.r.steins <ipb21.r.steins@pjvt.local>
2023-10-31 11:37:38 +0200

    sek

--- gherkin-IPb21-main/src/App.js
+++ gherkin-IPb21-main/src/App.js
@@ -1,8 +1,60 @@
+import 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi, Justiņ</h1>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Angliski</th>
+          <th>Latviski</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Feature</td>
+          <td>Funkcionālā iezīme</td>
+        </tr>
+        <tr>
+          <td>Background</td>
+          <td>Konteksts</td>
+        </tr>
+        <tr>
+          <td>Scenario</td>
+          <td>Scenārijs</td>
+        </tr>
+        <tr>
+          <td>Scenario Outline</td>
+          <td>Scenārijs pēc parauga</td>
+        </tr>
+        <tr>
+          <td>Examples</td>
+          <td>Piemēri</td>
+        </tr>
+        <tr>
+          <td>Given</td>
+          <td>Kad</td>
+        </tr>
+        <tr>
+          <td>When</td>
+          <td>Ja</td>
+        </tr>
+        <tr>
+          <td>Then</td>
+          <td>Tad</td>
+        </tr>
+        <tr>
+          <td>And</td>
+          <td>Un</td>
+        </tr>
+        <tr>
+          <td>Feature</td>
+          <td>Funkcionālā iezīme</td>
+        </tr>
+        <tr>
+          <td>Or</td>
+          <td>Bet</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
